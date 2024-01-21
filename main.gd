ead764c4f765b79e4669d36a3f0be22c59dea4a3
SeukWon Kang <kasworld@gmail.com>
2024-01-21 11:12:38 +0900

    add rotate, light

--- main.gd
+++ main.gd
@@ -47,6 +47,7 @@ func reset_camera_pos()->void:
 var camera_move = false
 func _process(_delta: float) -> void:
 	animove_step()
+	rot_by_accel()
 	var t = Time.get_unix_time_from_system() /-3.0
 	if camera_move:
 		$Camera3D.position = Vector3(sin(t)*sect_width/2 ,sect_width, cos(t)*sect_width/2  )
@@ -62,15 +63,72 @@ func _unhandled_input(event: InputEvent) -> void:
 			if camera_move == false:
 				reset_camera_pos()
 		else:
-			pass
+			light_state = (light_state+1)%3
+			light_on(light_state)
 
 	elif event is InputEventMouseButton and event.is_pressed():
 		pass
 
+func _notification(what: int) -> void:
+	# app resume on android
+	if what == NOTIFICATION_APPLICATION_RESUMED :
+		set_light_by_time()
 
+var oldvt = Vector2(0,-100)
+func rot_by_accel()->void:
+	var vt = Input.get_accelerometer()
+	if  vt != Vector3.ZERO :
+		oldvt = (Vector2(vt.x,vt.y) + oldvt).normalized() *100
+		var rad = oldvt.angle_to(Vector2(0,-1))
+		rotate_all(rad)
+	else :
+		vt = Input.get_last_mouse_velocity()/100
+		if vt == Vector2.ZERO :
+			vt = Vector2(0,-5)
+		oldvt = (Vector2(vt.x,vt.y) + oldvt).normalized() *100
+		var rad = oldvt.angle_to(Vector2(0,-1))
+		rotate_all(rad)
+
+func rotate_all(rad :float):
+	$AnalogClock3d.rotation.y = rad
+	$Calendar3d.rotation.y = rad
+
+var light_state = 0
+func set_light_by_time()->void:
+	var now = Time.get_datetime_dict_from_system()
+	if now["hour"] < 6 or now["hour"] >= 18 :
+		light_on(2)
+	else :
+		light_on(0)
+
+func light_on(s :int)->void:
+	match s %3 :
+		0: # all on
+			$OmniLight3D.visible = true
+			$DirectionalLight3D.visible = true
+		1:
+			$OmniLight3D.visible = true
+			$DirectionalLight3D.visible = false
+		2:
+			$OmniLight3D.visible = false
+			$DirectionalLight3D.visible = true
+
+var old_time_dict = Time.get_datetime_dict_from_system() # datetime dict
 var old_minute_dict = Time.get_datetime_dict_from_system() # datetime dict
 func _on_timer_timeout() -> void:
 	var time_now_dict = Time.get_datetime_dict_from_system()
 	if old_minute_dict["minute"] != time_now_dict["minute"]:
 		$AniMove3D.start_with_step(1)
 		old_minute_dict = time_now_dict
+
+	if old_time_dict["hour"] != time_now_dict["hour"]:
+		old_time_dict = time_now_dict
+		match time_now_dict["hour"]:
+			6:
+				light_on(0)
+			18:
+				light_on(2)
+			_:
+				pass
+
+
